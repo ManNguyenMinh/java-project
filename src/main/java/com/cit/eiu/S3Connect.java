c0a6a148f6e445d0cad57a31aa24296a6c7cba75
ManNguyenMinh <138440332+ImMinhMan@users.noreply.github.com>
2023-12-15 00:12:34 +0700

    commit

--- src/main/java/com/cit/eiu/S3Connect.java
+++ src/main/java/com/cit/eiu/S3Connect.java
@@ -5,12 +5,16 @@ import java.util.List;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
+import com.amazonaws.partitions.model.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -30,18 +34,22 @@ import javax.servlet.http.HttpServletResponse;
 
 public class S3Connect extends HttpServlet {
     public static void main(String[] args) {
-        AWSCredentials credentials = (AWSCredentials) new InstanceProfileCredentialsProvider(false);
+        // Specify the AWS region
+        // software.amazon.awssdk.regions.Region region = Region.AP_SOUTHEAST_2; // Change this to your desired region
 
-        AmazonS3 s3Client = AmazonS3ClientBuilder
-            .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
-            .withRegion(Regions.AP_NORTHEAST_2)
-            .build();
+        // Create an S3 client
+        S3Client s3 = S3Client.builder().region(Region.AP_SOUTHEAST_2).credentialsProvider(DefaultCredentialsProvider.create()).build();
 
-            // Display the bucket names
-            s3Client.listBuckets();
-            for (Bucket bucket : s3Client.listBuckets()) {
-                System.out.println("bucketName=" + bucket.getName());
+        try {
+            // List all S3 buckets in the account
+            ListBucketsResponse response = s3.listBuckets();
+            for (software.amazon.awssdk.services.s3.model.Bucket bucket : response.buckets()) {
+                System.out.println("Bucket Name: " + bucket.name());
             }
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
     }
 }
+
